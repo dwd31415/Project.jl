d8fd7307242c5951bec2437b47c7edf6c2f5c898
Adrian Dawid <adriandwd@gmail.com>
2016-05-26 17:12:04 +0200

    Add "add dependencies" functionAdd "add dependencies" function

--- src/Project.jl
+++ src/Project.jl
@@ -3,7 +3,7 @@ module Project
 import JSON
 # package code goes here
 
-export new
+export new,add
 
 function new(project_name::AbstractString,version::AbstractString,project_directory::AbstractString="pwd")
   if (project_directory == "pwd")
@@ -24,6 +24,27 @@ function new(project_name::AbstractString,version::AbstractString,project_direct
   write(project_file,json_string)
   close(project_file)
   print("Your project was created successfully.")
+end
+
+function add(package_name::AbstractString)
+  project_directory = pwd()
+  cd(project_directory)
+  if (isfile("./project.json"))
+    project_file = open("./project.json")
+    project_settings = JSON.parse(readall(project_file))
+    push!(project_settings["dependencies"],("pkg",package_name))
+    try
+      if (get(Pkg.installed(), package_name, 0) == 0)
+        Pkg.add(package_name)
+      end
+      project_file = open("./project.json","w")
+      json_string = JSON.json(project_settings)
+      write(project_file,json_string)
+    catch e
+      println(string("\x1b[31m","\x1b[1m","An error occured while installing the package $package_name via Pkg.add"))
+    end
+    close(project_file)
+  end
 end
 
 end # module
